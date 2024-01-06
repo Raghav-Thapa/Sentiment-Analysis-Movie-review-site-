5d250607a8207ccb2a2b371b697c42b107748faf
Raghav Thapa <raghavthapa7@gmail.com>
2024-01-06 22:31:55 +0545

    testing without adding movies id

--- backend/src/controller/sentiment.controller.js
+++ backend/src/controller/sentiment.controller.js
@@ -10,75 +10,132 @@ const Movie = require('../models/movie.model');
 
 class SentimentController {
 
-    addSentiments = async (req, res, next) => {
-        try{
-            
-            // const text = req.body.text;
-            const { text, movieId } = req.body;
-            const user = req.user;
-            // console.log(user)
+  addSentiments = async (req, res, next) => {
+    try{
 
-            const movie = await Movie.findById(movieId);
+        const text = req.body.text;
 
-            if (!movie) {
-              return res.status(404).json({ error: 'Movie not found' });
-            }
-      
-            // Run a Python script as a subprocess to make predictions
-            const pythonProcess = spawn('python', [
-              path.join(__dirname, '../../predict.py'), // Path to Python script
-              JSON.stringify(text), // Pass inputData as a JSON string
-            ]);
-          
-            // Collect predictions from the Python script
-            let predictions = '';
-            pythonProcess.stdout.on('data', (data) => {
-              predictions += data.toString();
-            });
+        // Run a Python script as a subprocess to make predictions
+        const pythonProcess = spawn('python', [
+          path.join(__dirname, '../../predict.py'), // Path to Python script
+          JSON.stringify(text), // Pass inputData as a JSON string
+        ]);
 
-            pythonProcess.on('close', async (code) => {
-                if (code === 0) {
-                  try {
-                    const result = JSON.parse(predictions);
-            
-                    // Save the sentiment analysis result to the database
-                    const sentimentResult = new SentimentResult({
-                      username: user.name,
-                      text,
-                      sentiment: result,
-                      moviename: movie.name,
-                      movie: movie._id,
-                    });
-                    console.log(sentimentResult)
-                    try {
-                      await sentimentResult.save();
-                      const responseObj = {
-                        text: sentimentResult.text,
-                        username: user.name,
-                        moviename: movie.name,
-                        sentiment: sentimentResult.sentiment,
-                        timestamp: sentimentResult.timestamp,
-                      };
-                      res.json(responseObj);
-                    } catch (error) {
-                      res.status(500).json({ error: 'Error saving sentiment result' });
-                    }
-            
-                  } catch (error) {
-                    res.status(500).json({ error: 'Prediction failed' });
-                  }
-                } else {
-                  res.status(500).json({ error: 'Prediction process exited with an error' });
+        // Collect predictions from the Python script
+        let predictions = '';
+        pythonProcess.stdout.on('data', (data) => {
+          predictions += data.toString();
+        });
+
+        pythonProcess.on('close', async (code) => {
+            if (code === 0) {
+              try {
+                const result = JSON.parse(predictions);
+
+                // Save the sentiment analysis result to the database
+                const sentimentResult = new SentimentResult({
+                  text,
+                  sentiment: result,
+                });
+                console.log(sentimentResult)
+                try {
+                  await sentimentResult.save();
+                  const responseObj = {
+                    text: sentimentResult.text,
+                    sentiment: sentimentResult.sentiment,
+                    timestamp: sentimentResult.timestamp,
+                  };
+                  res.json(responseObj);
+                } catch (error) {
+                  res.status(500).json({ error: 'Error saving sentiment result' });
                 }
-              });
+
+              } catch (error) {
+                res.status(500).json({ error: 'Prediction failed' });
+              }
+            } else {
+              res.status(500).json({ error: 'Prediction process exited with an error' });
+            }
+          });
 
 
-        }catch(exception){
-            console.log(exception)
-            next(exception)
-        }
-
+    }catch(exception){
+        console.log(exception)
+        next(exception)
     }
+
+}
+
+
+    // addSentiments = async (req, res, next) => {
+    //     try{
+            
+    //         // const text = req.body.text;
+    //         const { text, movieId } = req.body;
+    //         const user = req.user;
+    //         // console.log(user)
+
+    //         const movie = await Movie.findById(movieId);
+
+    //         if (!movie) {
+    //           return res.status(404).json({ error: 'Movie not found' });
+    //         }
+      
+    //         // Run a Python script as a subprocess to make predictions
+    //         const pythonProcess = spawn('python', [
+    //           path.join(__dirname, '../../predict.py'), // Path to Python script
+    //           JSON.stringify(text), // Pass inputData as a JSON string
+    //         ]);
+          
+    //         // Collect predictions from the Python script
+    //         let predictions = '';
+    //         pythonProcess.stdout.on('data', (data) => {
+    //           predictions += data.toString();
+    //         });
+
+    //         pythonProcess.on('close', async (code) => {
+    //             if (code === 0) {
+    //               try {
+    //                 const result = JSON.parse(predictions);
+            
+    //                 // Save the sentiment analysis result to the database
+    //                 const sentimentResult = new SentimentResult({
+    //                   username: user.name,
+    //                   text,
+    //                   sentiment: result,
+    //                   moviename: movie.name,
+    //                   movie: movie._id,
+    //                 });
+    //                 console.log(sentimentResult)
+    //                 try {
+    //                   await sentimentResult.save();
+    //                   const responseObj = {
+    //                     text: sentimentResult.text,
+    //                     username: user.name,
+    //                     moviename: movie.name,
+    //                     sentiment: sentimentResult.sentiment,
+    //                     timestamp: sentimentResult.timestamp,
+    //                   };
+    //                   res.json(responseObj);
+    //                 } catch (error) {
+    //                   res.status(500).json({ error: 'Error saving sentiment result' });
+    //                 }
+            
+    //               } catch (error) {
+    //                 res.status(500).json({ error: 'Prediction failed' });
+    //               }
+    //             } else {
+    //               res.status(500).json({ error: 'Prediction process exited with an error' });
+    //             }
+    //           });
+
+
+    //     }catch(exception){
+    //         console.log(exception)
+    //         next(exception)
+    //     }
+
+    // }
   
       getSentimentsByMovie = async (req, res, next) => {
           try {
